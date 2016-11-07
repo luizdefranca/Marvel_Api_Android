ff52079949871d789eac01d09e1e107482a2ce46
luizdefranca <luizramospe@hotmail.com>
2016-11-07 15:16:07 -0300

    Implementada o menu com a opcao de busca

--- app/src/main/java/com/example/android/marvelheroes/MainActivity.java
+++ app/src/main/java/com/example/android/marvelheroes/MainActivity.java
@@ -1,26 +1,27 @@
 package com.example.android.marvelheroes;
 
-import android.os.AsyncTask;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ListView;
 
 import com.example.android.marvelheroes.adapters.CharacterAdapter;
-import com.example.android.marvelheroes.http.CharactersParser;
 import com.example.android.marvelheroes.http.CharactersSearchTask;
 import com.example.android.marvelheroes.model.Character;
-import com.example.android.marvelheroes.model.Data;
 
-import java.io.IOException;
 import java.util.List;
 
-import static android.icu.lang.UCharacter.GraphemeClusterBreak.L;
-
-public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<Character>> {
+public class MainActivity extends AppCompatActivity
+        implements LoaderManager.LoaderCallbacks<List<Character>>, SearchView.OnQueryTextListener {
 
     ListView mListViewCharacters;
+    LoaderManager mLoaderManager;
+    String mquery;
 
 
     @Override
@@ -28,14 +29,15 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mListViewCharacters = (ListView) findViewById(R.id.list_characters);
-        LoaderManager loaderManager = getSupportLoaderManager();
-        loaderManager.initLoader(0, null, this);
+        mLoaderManager = getSupportLoaderManager();
+
+        mLoaderManager.initLoader(0, null, this);
     }
 
     @Override
     public Loader<List<Character>> onCreateLoader(int id, Bundle args) {
-
-        return new CharactersSearchTask(this, "iron");
+        String query =  args != null ? args.getString("q"): null;
+        return new CharactersSearchTask(this, query);
     }
 
     @Override
@@ -48,7 +50,28 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
 
     @Override
     public void onLoaderReset(Loader<List<Character>> loader) {
-
+        mLoaderManager.initLoader(0, null, this);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_search_bar, menu);
+        MenuItem searchItem = menu.findItem(R.id.search);
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+        searchView.setOnQueryTextListener(this);
+        return true;
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        Bundle bundle = new Bundle();
+        bundle.putString("q", query);
+        mLoaderManager.restartLoader(0,bundle, this);
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        return false;
+    }
 }
